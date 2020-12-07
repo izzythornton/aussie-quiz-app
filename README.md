d24b325a214d5ba148e3bfd41b2263792cdfe7fb
izzythornton <72430204+izzythornton@users.noreply.github.com>
2020-12-06 18:41:24 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-### Boilerplate app structure for Quiz App project
+Quiz app project with Bloc.io
--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
+/******** QUIZ QUESTION ARRAY ********/
+
+const STORE = {
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'Which of these nicknames is affectionately given to the Australian Shepherd?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'The Barkless Dog',
+        'The Butterfly Dog',
+        'The Velcro Dog',
+        'The Speedy Dog'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'The Velcro Dog'
     },
     {
-      question: 'What is the current year?',
+      question: 'What caused the Aussie popularity boom?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Excellence in sheep herding',
+        'Performing in rodeos',
+        'Recognition as an ideal family dog',
+        'All of the above'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'Performing in rodeos'
+    },
+    {
+      question: 'What kinds of jobs can an Aussie do?',
+      answers: [
+        'Herding',
+        'Service',
+        'Search and rescue',
+        'All of the above'
+      ],
+      correctAnswer: 'All of the above'
+    },
+    {
+      question: 'How many standard coat colors do Aussies have?',
+      answers: [
+        '4',
+        '2',
+        '6',
+        '3'
+      ],
+      correctAnswer: '4'
+    },
+    {
+      question: 'Some Australian Shepherds are born without a tail. What are the odds of that happening?',
+      answers: [
+        '50%',
+        '70%',
+        '20%',
+        '25%'
+      ],
+      correctAnswer: '20%'
     }
   ],
   quizStarted: false,
@@ -30,29 +58,154 @@ const store = {
   score: 0
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+/****************************************************/
+/************** START JAVASCRIPT FUNCTIONS *********/
+/**************************************************/
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
-// These functions return HTML templates
+function generateIntroPage() {
+  return `
+    <div class="intro">
+      <p>How much do you know about Aussies? This quiz will check your knowledge of the breed by going over a few fun facts!</p>
+      <button type="button" id="start-quiz" name="start-quiz">
+      <label for="start-quiz">Get Started</label>
+    </div>
+    `;
+}
+
+function generateStatus() {
+  return `
+      <div class="status-container">
+      <p class="question-counter">Question ${STORE.questionNumber}/5</p>
+        <p class="correct-answer-counter">Score: ${STORE.score}</p>
+      </div>
+    `;
+}
+
+function generatePossibleAnswers() {
+  let i = STORE.questionNumber;
+  const answersArray = STORE.questions[i].answers;
+  let html = "";
+  html += `
+    <div class="answer-container">
+    <form>
+      <input type="radio" name="possible-answer" id="${answersArray[0]}" value="${answersArray[0]}" required>
+      <label for="${answersArray[0]}">${answersArray[0]}</label><br>
+      <input type="radio" name="possible-answer" id="${answersArray[1]}" value="${answersArray[1]}" required>
+      <label for="${answersArray[1]}">${answersArray[1]}</label><br>
+      <input type="radio" name="possible-answer" id="${answersArray[2]}" value="${answersArray[2]}" required>
+      <label for="${answersArray[2]}">${answersArray[2]}</label><br>
+      <input type="radio" name="possible-answer" id="${answersArray[3]}" value="${answersArray[3]}" required>
+      <label for="${answersArray[3]}">${answersArray[3]}</label><br>
+    </form>
+    </div>
+    <form>
+      <input type="button" id="check-answer-button" name="check-answer-button" value="Check Answer" onclick="checkAnswer()">
+    </form>
+  `;
+  return html;
+} 
+
+function generateQuestion() {
+  let currentQuestion = "";
+  let i = STORE.questionNumber;
+  for (i; i < STORE.questions.length; i++) {
+    currentQuestion = STORE.questions[i].question;
+    return currentQuestion;
+  }
+}
+
+// NOT TESTED
+function generateFinalScore() {
+  return `
+      <div class="final-score">
+        <p>Awesome, you're all done! You answered ${STORE.score} out of 5 questions correctly.</p>
+        <p>Want to try again?</p>
+        <form id="try-again">
+          <input type="button" id="restart" name="restart" value="Click Here!" onclick="restartQuiz()">
+        </form>
+      </div>
+    `;
+}
+
+function correctAnswerFeedback() {
+  $(".quiz-container").html(`
+    <p>That's correct! Good job.</p>
+    <form>
+      <input type="button" id="next-question-button" name="next-question-button" value="Next Question" onclick="nextQuestion()">
+    </form>
+  `);
+}
+
+function incorrectAnswerFeedback() {
+  $(".quiz-container").html(`
+    <p>Not quite! That answer is incorrect.</p>
+    <p>The correct answer is ${STORE.questions[STORE.questionNumber].correctAnswer}.</p>
+    <form>
+      <input type="button" id="next-question-button" name="next-question-button" value="Next Question" onclick="nextQuestion()">
+    </form>
+  `);
+}
 
 /********** RENDER FUNCTION(S) **********/
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+// TO DO: Add condition for when quiz is completed
+function rendering() {
+  let html = "";
+  if (STORE.quizStarted === false) {
+    $(".quiz-container").html(generateIntroPage());
+      return;
+    } else if (STORE.questionNumber >= 0 && STORE.questionNumber < STORE.questions.length) {
+        html = generateStatus();
+        html += generateQuestion();
+        html += generatePossibleAnswers();
+        $(".quiz-container").html(html);
+    } else {
+        $(".quiz-container").html(generateFinalScore());
+  }
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
-// These functions handle events (submit, click, etc)
+function startQuiz() {
+  $(".quiz-container").on("click", "#start-quiz", function(event) {
+    event.preventDefault();
+    STORE.quizStarted = true;
+    rendering();
+  });
+}
+
+function checkAnswer() {
+  let selectedAnswer = $("input[name=possible-answer]:checked").val();
+  let currentQuestion = STORE.questionNumber;
+  let correctAnswer = STORE.questions[currentQuestion].correctAnswer;
+  if (selectedAnswer === correctAnswer) {
+    STORE.score++;
+    correctAnswerFeedback();
+  } else {
+    incorrectAnswerFeedback();
+  }
+  STORE.questionNumber++;
+}
+
+function nextQuestion() {
+  rendering();
+}
+
+function restartQuiz() {
+    STORE.quizStarted = false;
+    STORE.questionNumber = 0;
+    STORE.score = 0;
+    rendering();
+}
+
+// Confirmed this does properly render HTML at first
+function loadQuiz() {
+  rendering();
+  startQuiz();
+  nextQuestion();
+  restartQuiz();
+}
+
+$(loadQuiz);
